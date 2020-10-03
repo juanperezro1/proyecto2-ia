4e883823dac082732ab826a353439b7a8b8b9b0b
cam0104 <67967779+cam0104@users.noreply.github.com>
2020-10-03 11:32:16 -0500

    movimiento fichas

--- laberinto.py
+++ laberinto.py
@@ -32,46 +32,100 @@ mapa = [[1, 2, 6, 7, 4, 5, 8, 3],
         [6, 3, 4, 2, 8, 7, 1, 5],
         [3, 8, 5, 4, 7, 6, 2, 1]]
 
-matriz_fichas = [[1, 2, 3, 4, 5, 6, 7, 8],
+matriz_fichas_negras = [[1, 2, 3, 4, 5, 6, 7, 8],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
-                 [1, 2, 3, 4, 5, 6, 7, 8]]
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+matriz_fichas_blancas = [[0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [1, 2, 3, 4, 5, 6, 7, 8]]
 
 ventana = pygame.display.set_mode((ancho,alto))
 pygame.display.set_caption("Kamisado") 
 
+def movimiento_fichas():
+    
+
+    ficha_elegida = int(input("¿Cual ficha desea mover (1-8)?: "))
+    print(ficha_elegida)
+    print()
+    print("Ingrese la nueva posicion ")
+    pos_x = int(input("Posicion x: "))
+    pos_y = int(input("Posicion y: "))
+
+    posicion_ficha_marron = []
+
+    if(ficha_elegida == 1):
+
+        for i in range(8):
+            for j in range(8):
+                if matriz_fichas_blancas[i][j] == 1:
+
+                    posicion_ficha_marron.append(i)
+                    posicion_ficha_marron.append(j)
+        
+        color_matriz = mapa[posicion_ficha_marron[0]][posicion_ficha_marron[1]]
+        actualizar_mapa(posicion_ficha_marron,color_matriz)
+
+        print(color_matriz)
+        print("pos_color",color_matriz)
+        print(posicion_ficha_marron)
+
+        ficha_blanca_mo = ficha_blanca_marron(pos_x * tamano_cuadros, pos_y* tamano_cuadros)
+        ficha_blanca_mo.dibujar_ficha(ventana)
+        
+    if(ficha_elegida == 2):
+
+        ficha_blanca_ve = ficha_blanca_verde(pos_x * tamano_cuadros, pos_y* tamano_cuadros)
+        ficha_blanca_ve.dibujar_ficha(ventana)
+
+def actualizar_mapa(pos_ficha,color):
+    
+    if (color == 3):
+        
+        pygame.draw.rect(ventana,marron,pygame.Rect(pos_ficha[1] * tamano_cuadros, pos_ficha[0] *tamano_cuadros,tamano_cuadros,tamano_cuadros))
+        pygame.display.update()
+        
 def construir_mapa(mapa):
     x = 0
     y = 0 
     for fila in range(8):
-        for muro in range(8):
-            if mapa[fila][muro] == 1:
+        for columna in range(8):
+            if mapa[fila][columna] == 1:
                 pygame.draw.rect(ventana,naranja,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 2:
+            if mapa[fila][columna] == 2:
                 pygame.draw.rect(ventana,azul,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 3:
+            if mapa[fila][columna] == 3:
                 pygame.draw.rect(ventana,marron,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 4:
+            if mapa[fila][columna] == 4:
                 pygame.draw.rect(ventana,amarillo,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 5:
+            if mapa[fila][columna] == 5:
                 pygame.draw.rect(ventana,rojo,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 6:
+            if mapa[fila][columna] == 6:
                 pygame.draw.rect(ventana,morado,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 7:
+            if mapa[fila][columna] == 7:
                 pygame.draw.rect(ventana,rosado,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
-            if mapa[fila][muro] == 8:
+            if mapa[fila][columna] == 8:
                 pygame.draw.rect(ventana,verde,pygame.Rect(x,y,tamano_cuadros,tamano_cuadros))
             x += 80
         x = 0
         y += 80
 
 def construir_fichas_blancas(matriz_fichas):
+
     for i in range(8):
+
         if matriz_fichas[i] == 8:
             ficha_blanca_n = ficha_blanca_naranja(i * tamano_cuadros, i * tamano_cuadros)
             ficha_blanca_n.dibujar_ficha(ventana)
@@ -301,19 +355,27 @@ class ficha_blanca_marron(pygame.sprite.Sprite):
     def dibujar_ficha(self, superficie):
         superficie.blit(self.imagenMuro, self.rect)
 
+ventana.fill(negro)
+
+construir_mapa(mapa)
+construir_fichas_negras(matriz_fichas_negras[0])
+construir_fichas_blancas(matriz_fichas_blancas[7])
+
+pygame.display.update()
+movimiento_fichas()
+pygame.display.update()
+
+
 while True:
     
-    ventana.fill(negro)
-    construir_mapa(mapa)
-    construir_fichas_negras(matriz_fichas[0])
-    construir_fichas_blancas(matriz_fichas[7])
-
-    pygame.display.update()
+    #pygame.display.update()
 
     for evento in pygame.event.get():
         if evento.type == QUIT:
             pygame.quit()
             sys.exit()
+    
+  
 
 
 
